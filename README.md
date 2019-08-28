2b3223e272f12d38177e690a5630aa0aa9608076
MuMu <raomengyun@gmail.com>
2019-08-28 13:57:03 +0800

    工具调整为检测项目根目录中是否含有asset_config并自动触发

--- README.md
+++ README.md
@@ -2,16 +2,10 @@
 
 This a Extension of [auto_access](https://github.com/raomengyun/auto_assets)
 
-# 1. 解决的问题
-
-1. Flutter 以文件夹形式声明资源时。只添加 2x 3x 图，Flutter 的搜索系统是无法查找出对应资源的。
-2. Flutter 资源是直接拷贝至 Native 端的，需要进行压缩，工具: imagemin。
-3. 资源强类型化是更好的资源管理方式，因为 Dart 是静态强类型语言，我们可以借助这个特征来自动分析未使用的资源，方便未来的资源管理。
-
-# 2. 实施
+# 1. 功能
 
 1. 自动监听资源文件夹，自动生成 assets 声明并插入 pubspec.yaml 文件中.
-2. 自动压缩图片资源。
+2. 自动压缩图片资源（jpg/jpeg/png/svg）。
 3. 使用类型来声明并且引用图片资源，例如：
    ```dart
    class Assets {
@@ -22,9 +16,9 @@ This a Extension of [auto_access](https://github.com/raomengyun/auto_assets)
 
 # 4. 工具使用
 
-## 4.1. 项目配置
+> 根目录中是否有 `assets_config.json` 时会自动开启。
 
-在项目根目录下建一个 `assets_config.json` 文件，文件内容：
+`assets_config.json`配置内容：
 
 ```json
 {
@@ -35,11 +29,3 @@ This a Extension of [auto_access](https://github.com/raomengyun/auto_assets)
 
 - `assets` 代表资源的根目录
 - `code` 代表自动生成的代码的根目录
-
-## 4.2. 执行命令
-
-`Command`+`Shift`+`P`，选择 `Auto Assets`。
-
-![auto_assets](https://raw.githubusercontent.com/raomengyun/auto_assets_vscode_extension/master/images/auto_assets.png)
-
-执行命令后，资源文件夹内所有的变动都会自动更新到自动生成的代码上。
--- extension.js
+++ extension.js
@@ -7,38 +7,23 @@ var autoAssets = undefined;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "auto-assets" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "extension.autoAssets",
-    function() {
-      // The code you place here will be executed every time your command is executed
-      const rootPath = vscode.workspace.rootPath;
-      if (!rootPath) {
-        vscode.window.showInformationMessage(
-          "You should use this extension in a workspace!"
-        );
-        return;
-      }
-      autoAssets = new AutoAssets(rootPath);
-      try {
-        autoAssets.start();
-      } catch (error) {
-        vscode.window.showInformationMessage(error.message);
-      }
-    }
-  );
-
-  context.subscriptions.push(disposable);
+function activate() {
+  // Use the console to output diagnostic information (console.log) and errors
+  // (console.error) This line of code will only be executed once when your
+  // extension is activated
+  const rootPath = vscode.workspace.rootPath;
+  if (!rootPath) {
+    vscode.window.showInformationMessage(
+      "You should use this extension in a workspace!"
+    );
+    return;
+  }
+  autoAssets = new AutoAssets(rootPath);
+  try {
+    autoAssets.start();
+  } catch (error) {
+    vscode.window.showInformationMessage(error.message);
+  }
 }
 exports.activate = activate;
 
